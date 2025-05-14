4a30981048cc9c74ce9550a973bb560ecfcbb19f
Julio Gonzales <dg2007xd@gmail.com>
2025-05-14 13:17:06 -0500

    septimo commit creo

--- src/home/Productos.tsx
+++ src/home/Productos.tsx
@@ -2,19 +2,26 @@ import './Productos.css'
 import { Articulo } from '../types/Articulo'
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { API_URL } from '../utils';
 
-function Noticias() {
+interface ProductosProps {
+    codigoCategoria: number;
+}
+
+function Noticias({ codigoCategoria }: ProductosProps) {
+    console.log(codigoCategoria);
 
     const [categoriaSeleccionada, setCategoriaSeleccionada] = useState("primero");
 
     const [listaArticulos, setListaArticulos] = useState<Articulo[]>([]);
 
     useEffect(() => {
-        leerServicio();
-    }, []);
+        leerServicio(codigoCategoria);
+    }, [codigoCategoria]);
 
-    const leerServicio = () => {
-        fetch("http://localhost/PHPgym/productos.php")
+    const leerServicio = async (id: number) => {
+        /*
+        fetch(API_URL + "productos.php")
             .then(response => response.json())
             .then((data: Articulo[]) => {
                 console.log(data);
@@ -23,6 +30,15 @@ function Noticias() {
             .catch((error) => {
                 console.error("Error consultando datos:", error);
             });
+            */
+        try {
+            const response = await fetch(API_URL + "productos.php?id=" + id);
+            const data: Articulo[] = await response.json();
+            console.log(data);
+            setListaArticulos(data);
+        } catch (error) {
+            console.log("Error consultando datos:", error);
+        }
     }
 
     const renderStars = (rating: number) => {
@@ -39,23 +55,27 @@ function Noticias() {
     const dibujarItems = () => {
         return (
             <div className='row center'>
-                {
-                    listaArticulos.map(item =>
+                {listaArticulos.map(item => {
+                    const nombre = String(item.nombre)
+                    const precio = Number(item.precio)
+                    return (
                         <div className='col-3' key={item.id}>
-                            <div className='sec-pro'>   
+                            <div className='sec-pro'>
                                 <div id='back-img' className='center container-fluid'>
-                                    <img src={"http://localhost/PHPgym/" + item.imagen} className="img-fluid ps-3 pe-3 pt-3 pb-3" alt="..." />
+                                    <img src={item.imagen === null
+                                        ? API_URL + "img/nofoto.jpg"
+                                        : API_URL + item.imagen} className="img-fluid ps-3 pe-3 pt-3 pb-3" alt="..." />
                                 </div>
-
-                                <p className='center ms-4 me-4 fs-6 fsnombre'>{item.nombre}</p>
+                                
+                                <p className='center ms-4 me-4 fs-6 fsnombre'>{nombre || "Nombre no disponible"}</p>
                                 <div className="center">
                                     {renderStars(item.rating)}
                                 </div>
-                                <p className='precio-produc center'>{item.precio}</p>
+                                <p className='precio-produc center'>S/ {precio}</p>
                             </div>
                         </div>
-                    )
-                }
+                    )}
+                )}
             </div>
         );
     };
